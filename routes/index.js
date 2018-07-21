39ce4555366ec18f0b573445b6ee4828c9d367b7
rarepep8 <alex.wong.utsc@gmail.com>
2018-07-21 17:48:59 -0400

    refactored sql code

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,15 @@ app.use(express.static('/../public'));
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: './views/'});
 });
-database.connection.getConnection(function(err) {
-  if (err) throw err;
-  database.connection.query("SELECT * FROM user", function (err, result, fields) {
-    if(err) throw err;
-    console.log(result);
-    document.getElementById('test-text').innerHTML=result;
+app.get('/login', function(req, res) {
+  database.connection.getConnection(function(err) {
+    if (err) throw err;
+    database.connection.query("SELECT * FROM user", function (err, result, fields) {
+      if(err) throw err;
+      console.log(result);
+      res.send(result);
+    });
   });
 });
+
 module.exports = app;
