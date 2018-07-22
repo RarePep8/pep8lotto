44a57340e7f9ab29e3a1c4ccbfffb879fd7ad3ec
rarepep8 <alex.wong.utsc@gmail.com>
2018-07-22 18:55:02 -0400

    Updated

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,17 @@ app.use(express.static('/../public'));
 app.get('/', function(req, res) {
   res.sendFile('index.html', {root: './views/'});
 });
+app.get('/earn', function(req, res){
+
+});
 app.get('/login', function(req, res) {
   database.connection.getConnection(function(err) {
     if (err) throw err;
-    database.connection.query("UPDATE user SET balance = balance + 1 WHERE user_id = 2;");
     var username = req.param('username');
     var password = req.param('password');
     database.connection.query("SELECT password FROM user WHERE username=" + username, function (err, result, fields) {
       if(err) throw err;
+      database.connection.release();
       console.log(result);
       result_string = (result.length == 0) ? "" : result[0].password;
       console.log(result_string);
