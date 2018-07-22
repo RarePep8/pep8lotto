800128912571fcc50e1aa7b667fbf61c1efd2f49
rarepep8 <alex.wong.utsc@gmail.com>
2018-07-22 19:02:09 -0400

    Updated

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 var mysql = require('mysql');
-var connection= mysql.createPool(process.env.DATABASE_URL);
-connection.getConnection(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+var pool= mysql.createPool(process.env.DATABASE_URL);
+// pool.getConnection(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
 
-module.exports.connection = connection;
+module.exports.pool = pool;
