9a89f007a0b35b271c543c4318f11b2e20317451
RarePep8 <alex.wong.utsc@gmail.com>
2018-08-01 14:35:03 -0400

    Updated

--- routes/index.js
+++ routes/index.js
@@ -13,48 +13,51 @@ function authenticate(req, res, action) {
     var password = req.param('password');
     var authenticated = false;
     database.pool.getConnection(function(err, connection) {
-        if (err) throw err;
-        connection.query("SELECT * FROM user WHERE username=" + username, function(err, result, fields) {
             if (err) throw err;
-            connection.release();
-            var result_string = (result.length == 0) ? "" : result[0].password;
-            var response = {
-                "verified": (result_string != "") && (result_string == password)
-            };
-            var response = {
-                authenticated: false,
-                balance: null
-            }
-            response.authenticated = (result_string != "") && (result_string == password);
-            console.log(response.authenticated);
-            if(action == "login") {
-                res.send(response.authenticated);
-            } else if (action == "get-balance"){
-                if(response.authenticated) {
-                    response.balance = result[0].balance;
-                    console.log(response);
-                }
-                res.send(response);
-            } else if (action == "earn") {
-                if(response.authenticated){
-                    connection.query("UPDATE user SET balance = balance + 1 where username=" + username, function(err, result, fields) {
-                        if (err) throw err;
-                        connection.release();
-                        console.log(response);
+            connection.query("SELECT * FROM user WHERE username=" + username, function(err, result, fields) {
+                    if (err) throw err;
+                    connection.release();
+                    var result_string = (result.length == 0) ? "" : result[0].password;
+                    var response = {
+                        "verified": (result_string != "") && (result_string == password)
+                    };
+                    var response = {
+                        authenticated: false,
+                        balance: null
+                    }
+                    response.authenticated = (result_string != "") && (result_string == password);
+                    console.log(response.authenticated);
+                    if (action == "login") {
                         res.send(response.authenticated);
+                    } else if (action == "get-balance") {
+                        if (response.authenticated) {
+                            response.balance = result[0].balance;
+                            console.log(response);
+                        }
+                        res.send(response);
+                    } else if (action == "earn") {
+                        if (response.authenticated) {
+                            database.pool.getConnection(function(err, connection) {
+                                    if (err) throw err;
+                                    connection.query("UPDATE user SET balance = balance + 1 where username=" + username, function(err, result, fields) {
+                                        if (err) throw err;
+                                        connection.release();
+                                        console.log(response);
+                                        res.send(response.authenticated);
+                                    });
+                                });
+                            }
+                        }
                     });
-                }
-            }
-        });
+            });
+    }
+    app.get('/earn', function(req, res) {
+        authenticate(req, res, "earn");
     });
-}
-app.get('/earn', function(req, res) {
-    authenticate(req, res, "earn");
-});
-app.get('/login', function(req, res) {
-    authenticate(req, res, "login");
-});
-app.get('/get-balance', function(req, res) {
-    authenticate(req, res, 'get-balance');
-});
-module.exports = app;
+    app.get('/login', function(req, res) {
+        authenticate(req, res, "login");
+    });
+    app.get('/get-balance', function(req, res) {
+        authenticate(req, res, 'get-balance');
+    });
+    module.exports = app;
