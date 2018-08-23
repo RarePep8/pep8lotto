efd208036a7cf29cead068278a1520b42d98cea6
rarepep8 <alex.wong.utsc@gmail.com>
2018-08-23 17:18:42 -0400

    Updated

--- public/app.js
+++ public/app.js
@@ -1,103 +1,110 @@
 window.onload = function() {
-  var horizonItems = [{"name":"Code Red",
-                          "url":{"factoryNew":"https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXH5ApeO4YmlhxYQknCRvCo04DEVlxkKgpot7HxfDhjxszJegJM6dO4q5KCk_LmDLfYkWNF18lwmO7Eu9Wn2A3l8kE-Zmj3d4LAIQ82YFzWqFW5xem70MW078jNyXUysycltnfD30vgEgITXkM/360fx360f"}}];
-    console.log(horizonItems[0].url.factoryNew);
-    var curr_username;
-    var curr_password;
-    var curr_authenticated;
-    /**
-    console.log(day);
-    console.log(year);
-    **/
+  console.log(items[0].url);
+  var curr_username;
+  var curr_password;
+  var curr_authenticated;
+  /**
+  console.log(day);
+  console.log(year);
+  **/
 
-    function display_balance() {
-        var obj = JSON.parse(this.responseText);
-        document.getElementById('coin-count-text').innerText = obj.balance;
-        document.getElementById('coin-count-text').style.display = "inline-block";
-        document.getElementById('pep8coin-count').style.display = "inline-block";
-        console.log(obj.balance);
-        console.log(this.responseText);
+  function display_balance() {
+    var obj = JSON.parse(this.responseText);
+    document.getElementById('coin-count-text').innerText = obj.balance;
+    document.getElementById('coin-count-text').style.display = "inline-block";
+    document.getElementById('pep8coin-count').style.display = "inline-block";
+    console.log(obj.balance);
+    console.log(this.responseText);
+  }
+
+
+
+  const verifyPassword = function() {
+    if (this.responseText == 'true') {
+      curr_username = document.getElementById('username').value;
+      curr_password = document.getElementById('password').value;
+      curr_authenticated = true;
+      document.getElementById('logged-in-message').innerText = "Hi " +
+        curr_username;
+      document.getElementById('login-fields').style.display = "none";
+      document.getElementById('login-button').onclick = logout;
+      document.getElementById('login-button').innerText = "Logout";
+      update_balance();
     }
 
-
-
-
-
-    const verifyPassword = function() {
-        if (this.responseText == 'true') {
-            curr_username = document.getElementById('username').value;
-            curr_password = document.getElementById('password').value;
-            curr_authenticated = true;
-            document.getElementById('logged-in-message').innerText = "Hi " + curr_username;
-            document.getElementById('login-fields').style.display = "none";
-            document.getElementById('login-button').onclick = logout;
-            document.getElementById('login-button').innerText = "Logout";
-            update_balance();
-        }
-
-    }
-    var login = function() {
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        const request = new XMLHttpRequest();
-        request.addEventListener('load', verifyPassword);
-        request.open('get', '/login?username=\"' + username + '\"&password=' + password);
-        request.send();
-    }
-    var logout = function() {
-        curr_authenticated = false;
-        curr_username = null;
-        curr_password = null;
-        document.getElementById('logged-in-message').innerText = ""
-        document.getElementById('login-fields').style.display = "inline-block";
-        document.getElementById('username').value = "";
-        document.getElementById('password').value = "";
-        document.getElementById('login-button').onclick = login;
-        document.getElementById('login-button').innerText = "Login";
-        document.getElementById('coin-count-text').style.display = "none";
-        document.getElementById('pep8coin-count').style.display = "none";
-    }
-    console.log("yeet");
+  }
+  var login = function() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', verifyPassword);
+    request.open('get', '/login?username=\"' + username + '\"&password=' +
+      password);
+    request.send();
+  }
+  var logout = function() {
+    curr_authenticated = false;
+    curr_username = null;
+    curr_password = null;
+    document.getElementById('logged-in-message').innerText = ""
+    document.getElementById('login-fields').style.display = "inline-block";
+    document.getElementById('username').value = "";
+    document.getElementById('password').value = "";
     document.getElementById('login-button').onclick = login;
+    document.getElementById('login-button').innerText = "Login";
+    document.getElementById('coin-count-text').style.display = "none";
+    document.getElementById('pep8coin-count').style.display = "none";
+  }
+  console.log("yeet");
+  document.getElementById('login-button').onclick = login;
 
 
-    var username_input = document.getElementById('username');
-    var password_input = document.getElementById('password');
+  var username_input = document.getElementById('username');
+  var password_input = document.getElementById('password');
 
-    function enter_press_login(event) {
-        console.log("yah yeet");
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Number 13 is the "Enter" key on the keyboard
-        if (event.keyCode === 13) {
-            // Trigger the button element with a click
-            document.getElementById("login-button").click();
-        }
+  function enter_press_login(event) {
+    console.log("yah yeet");
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Trigger the button element with a click
+      document.getElementById("login-button").click();
     }
-    // Execute a function when the user releases a key on the keyboard
-    username_input.addEventListener("keyup", function(event) {
-        enter_press_login(event);
-    });
-    password_input.addEventListener("keyup", function(event) {
-        enter_press_login(event);
-    });
+  }
+  // Execute a function when the user releases a key on the keyboard
+  username_input.addEventListener("keyup", function(event) {
+    enter_press_login(event);
+  });
+  password_input.addEventListener("keyup", function(event) {
+    enter_press_login(event);
+  });
 
-    // Query user balance constantly
-    function update_balance() {
-        const request = new XMLHttpRequest();
-        request.addEventListener('load', display_balance);
-        request.open('get', '/get-balance?username=\"' + curr_username + '\"&password=' + curr_password);
-        request.send();
-    }
-    function earn() {
-        const request = new XMLHttpRequest();
-        request.addEventListener('load', update_balance);
-        request.open('get', '/earn?username=\"' + curr_username + '\"&password=' + curr_password);
-        request.send();
-    }
-    function test() {
-      console.log("You got Awp | Dragon Lore!!!");
-    }
-    document.getElementById('doubleup').onclick = earn;
-    //document.getElementById('horizon-case').onclick = test;
+  // Query user balance constantly
+  function update_balance() {
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', display_balance);
+    request.open('get', '/get-balance?username=\"' + curr_username +
+      '\"&password=' + curr_password);
+    request.send();
+  }
+
+  function earn() {
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', update_balance);
+    request.open('get', '/earn?username=\"' + curr_username + '\"&password=' +
+      curr_password);
+    request.send();
+  }
+
+  function openBasic() {
+    console.log("You got Awp | Dragon Lore!!!");
+    const request = new XMLHttpRequest();
+    request.addEventListener('load', update_balance);
+    request.open('get', '/open-basic?username=\"' + curr_username +
+      '\"&password=' + curr_password);
+    request.send();
+  }
+  document.getElementById('doubleup').onclick = earn;
+  document.getElementById('buy-basic-button').onclick = openBasic;
 };
--- routes/index.js
+++ routes/index.js
@@ -3,70 +3,132 @@ var app = express()
 var database = require('../database');
 app.use(express.static('/../public'));
 app.get('/', function(req, res) {
-    res.sendFile('index.html');
+  res.sendFile('index.html');
 });
+var basicUncommonItems = [{
+  "name": "Slime Green Block",
+  "url": "img/block_1.png"
+}, {
+  "name": "Yellow Block",
+  "url": "img/block_2.png"
+}, {
+  "name": "Green Block",
+  "url": "img/block_3.png"
+}, {
+  "name": "Blue Block",
+  "url": "img/block_4.png"
+}, {
+  "name": "Purple Block",
+  "url": "img/block_5.png"
+}, {
+  "name": "Pink Block",
+  "url": "img/block_6.png"
+}, {
+  "name": "Cyan Block",
+  "url": "img/block_7.png"
+}];
+var basicRareItems = [{
+  "name": "Slime Green Checker Block",
+  "url": "img/block_8.png"
+}, {
+  "name": "Yellow Checker Block",
+  "url": "img/block_9.png"
+}, {
+  "name": "Green Checker Block",
+  "url": "img/block_10.png"
+}, {
+  "name": "Blue Checker Block",
+  "url": "img/block_11.png"
+}, {
+  "name": "Purple Checker Block",
+  "url": "img/block_12.png"
+}, {
+  "name": "Pink Checker Block",
+  "url": "img/block_13.png"
+}, {
+  "name": "Cyan Checker Block",
+  "url": "img/block_14.png"
+}];
 const double_query = "UPDATE user SET balance = balance *2 where username=";
 const halve_query = "UPDATE user SET balance = balance /2 where username=";
-function authenticate(req, res, action) {
-    var username = req.param('username');
-    var password = req.param('password');
-    var authenticated = false;
-    database.pool.getConnection(function(err, connection) {
-            if (err) throw err;
-            connection.query("SELECT * FROM user WHERE username=" + username, function(err, result, fields) {
-                    if (err) throw err;
-                    connection.release();
-                    console.log(result);
-                    var result_string = (result.length == 0) ? "" : result[0].password;
-                    var response = {
-                        "verified": (result_string != "") && (result_string == password)
-                    };
-                    var response = {
-                        authenticated: false,
-                        balance: null
-                    }
-                    response.authenticated = (result_string != "") && (result_string == password);
-                    console.log(response.authenticated);
-                    if (action == "login") {
-                        res.send(response.authenticated);
-                    } else if (action == "get-balance") {
-                        if (response.authenticated) {
-                            response.balance = result[0].balance;
-                            console.log(response);
-                        }
-                        res.send(response);
-                    } else if (action == "earn") {
-                        if (response.authenticated) {
-                            var chosen_query = halve_query;
-                            if(Math.random() > 0.5){
-                                chosen_query = double_query;
-                            }
-                            database.pool.getConnection(function(err, connection) {
-                                    if (err) throw err;
-                                    connection.query(chosen_query + username, function(err, result, fields) {
-                                        if (err) throw err;
-                                        connection.release();
-                                        console.log(response);
-                                        res.send(response.authenticated);
-                                    });
-                                });
-                            }
-                        } else if (action == "open-horizon" && response.authenticated) {
 
-                        }
-                    });
+function authenticate(req, res, action) {
+  var username = req.param('username');
+  var password = req.param('password');
+  var authenticated = false;
+  database.pool.getConnection(function(err, connection) {
+    if (err) throw err;
+    connection.query("SELECT * FROM user WHERE username=" + username,
+      function(err, result, fields) {
+        if (err) throw err;
+        connection.release();
+        console.log(result);
+        var result_string = (result.length == 0) ? "" : result[0].password;
+        var response = {
+          "verified": (result_string != "") && (result_string ==
+            password)
+        };
+        var response = {
+          authenticated: false,
+          balance: null,
+          itemName: null,
+          itemUrl: null
+        }
+        response.authenticated = (result_string != "") && (result_string ==
+          password);
+        console.log(response.authenticated);
+        if (action == "login") {
+          res.send(response.authenticated);
+        } else if (action == "get-balance") {
+          if (response.authenticated) {
+            response.balance = result[0].balance;
+            console.log(response);
+          }
+          res.send(response);
+        } else if (action == "earn") {
+          if (response.authenticated) {
+            var chosen_query = halve_query;
+            if (Math.random() > 0.5) {
+              chosen_query = double_query;
+            }
+            database.pool.getConnection(function(err, connection) {
+              if (err) throw err;
+              connection.query(chosen_query + username, function(err,
+                result, fields) {
+                if (err) throw err;
+                connection.release();
+                console.log(response);
+                res.send(response.authenticated);
+              });
             });
-    }
-    app.get('/earn', function(req, res) {
-        authenticate(req, res, "earn");
-    });
-    app.get('/login', function(req, res) {
-        authenticate(req, res, "login");
-    });
-    app.get('/get-balance', function(req, res) {
-        authenticate(req, res, 'get-balance');
-    });
-    app.get('/open-horizon', function(req, res) {
-        authenticate(req, res, 'open-horizon');
-    });
-    module.exports = app;
+          }
+        } else if (action == "open-basic" && response.authenticated) {
+          var rarityInt = Math.random();
+          if (randInt < 0.5) {
+            var itemInt = Math.floor(Math.random() * 7);
+            response.itemName = basicUncommonItems[itemInt].name;
+            response.itemUrl = basicUncommonItems[itemInt].url;
+            console.log("You got " + response.itemName);
+          } else {
+            var itemInt = Math.floor(Math.random() * 7);
+            response.itemName = basicRareItems[itemInt].name;
+            response.itemUrl = basicRareItems[itemInt].url;
+            console.log("You got " + response.itemName);
+          }
+        }
+      });
+  });
+}
+app.get('/earn', function(req, res) {
+  authenticate(req, res, "earn");
+});
+app.get('/login', function(req, res) {
+  authenticate(req, res, "login");
+});
+app.get('/get-balance', function(req, res) {
+  authenticate(req, res, 'get-balance');
+});
+app.get('/open-basic', function(req, res) {
+  authenticate(req, res, 'open-basic');
+});
+module.exports = app;
